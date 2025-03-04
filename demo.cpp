b2a71778fbd81a82250d0ea52723a4d2e842e0e9
Rushikesh-lahane <lahanerushi10@gmail.com>
2025-03-04 22:57:21 +0530

    first commit

---
+++ demo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool isPalindrome(string str) {
+    string original = str;
+    reverse(str.begin(), str.end()); // Reverse the string
+    return original == str; // Check if reversed string matches original
+}
+
+int main() {
+    string input;
+    cout << "Enter a string: ";
+    cin >> input;
+
+    if (isPalindrome(input)) {
+        cout << "The string is a palindrome!" << endl;
+    } else {
+        cout << "The string is NOT a palindrome!" << endl;
+    }
+
+    return 0;
+}
